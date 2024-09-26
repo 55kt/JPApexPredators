39880601bab3de69aae6e8f99a56b0ba41b76f3e
Vlad <160039489+55kt@users.noreply.github.com>
2024-09-26 19:14:09 +0300

    Filter Menu

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -5,8 +5,11 @@ struct ContentView: View {
     let predators = Predators()
     @State var searchText = ""
     @State var alphabetical = false
+    @State var currentSelection = PredatorType.all
     
     var filteredDinos: [ApexPredator] {
+        predators.filter(by: currentSelection)
+        
         predators.sort(by: alphabetical)
         
         return predators.search(for: searchText)
@@ -59,6 +62,17 @@ struct ContentView: View {
                     } label: {
                         Image(systemName: alphabetical ? "textformat" : "film")
                             .symbolEffect(.bounce, value: alphabetical)
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu {
+                        Picker("Filter", selection: $currentSelection.animation()) {
+                            ForEach(PredatorType.allCases) { type in
+                                Label(type.rawValue.capitalized, systemImage: type.icon)
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "slider.horizontal.3")
                     }
                 }
             }
--- JPApexPredators/Predators.swift
+++ JPApexPredators/Predators.swift
@@ -2,6 +2,7 @@ import Foundation
 
 class Predators {
     // MARK: - Properties
+    var allApexPredators: [ApexPredator] = []
     var apexPredators: [ApexPredator] = []
     
     // MARK: - Initializers
@@ -18,7 +19,8 @@ class Predators {
                 let data = try Data(contentsOf: url)
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
-                apexPredators = try decoder.decode([ApexPredator].self, from: data)
+                allApexPredators = try decoder.decode([ApexPredator].self, from: data)
+                apexPredators = allApexPredators
             } catch {
                 print("Error decoding JSON data: \(error)")
             }
@@ -42,6 +44,16 @@ class Predators {
                 predator1.name < predator2.name
             } else {
                 predator1.id < predator2.id
+            }
+        }
+    }
+    
+    func filter(by type: PredatorType) {
+        if type == .all {
+            apexPredators = allApexPredators
+        } else {
+            apexPredators = allApexPredators.filter { predator in
+                predator.type == type
             }
         }
     }
